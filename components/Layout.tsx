f60e012e89b26785a60ac2c34fd258e25415bc21
Andrew Heyer <andrewbheyer@gmail.com>
2022-08-24 13:37:54 -0700

    adding pages, and routing

---
+++ components/Layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from "next/head";
+
+const Layout = (props) => {
+    return (
+        <>
+            <Head>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+                      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+                      crossOrigin="anonymous"/>
+            </Head>
+
+            <div className="container">
+                {props.children}
+            </div>
+        </>
+    );
+};
+
+export default Layout;
