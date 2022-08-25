e5174b8c99a25d3984105a7e81c8915017917950
Andrew Heyer <andrewbheyer@gmail.com>
2022-08-25 00:16:49 -0700

    order confirmation functioning - bug with mailhog

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Layout from "../components/Layout";
+import {useRouter} from "next/router";
+import axios from "axios";
+import constants from "../constants";
 
 const Success = () => {
+    const router = useRouter()
+    const {source} = router.query;
+
+    useEffect( () => {
+        if (source !== undefined) {
+            (
+                async () => {
+                    await axios.post(`${constants.endpoint}/orders/confirm`, {
+                        source
+                    })
+                }
+            )()
+        }
+    })
+
     return (
         <Layout>
             <div className="py-5 text-center">
